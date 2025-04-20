ed6e16f10c33bbffd51c71198a77556cad519a49
Felipe Vilela <felipe.vilela01@outlook.com>
2025-04-20 20:17:31 -0300

    feat: Adicionando id no resumo

--- src/main/java/com/vilela/felipe/api_2025/model/dto/DadosConsertoResumo.java
+++ src/main/java/com/vilela/felipe/api_2025/model/dto/DadosConsertoResumo.java
@@ -2,10 +2,10 @@ package com.vilela.felipe.api_2025.model.dto;
 
 import com.vilela.felipe.api_2025.model.entity.Conserto;
 
-public record DadosConsertoResumo(String dataEntrada, String dataSaida, String nome, String marca, String modelo) {
+public record DadosConsertoResumo(Long id, String dataEntrada, String dataSaida, String nome, String marca, String modelo) {
 
     public DadosConsertoResumo(Conserto conserto) {
-        this(conserto.getDataEntrada(), conserto.getDataSaida(), conserto.getMecanico().getNome(),
+        this(conserto.getId(), conserto.getDataEntrada(), conserto.getDataSaida(), conserto.getMecanico().getNome(),
                 conserto.getVeiculo().getMarca(), conserto.getVeiculo().getModelo());
     }
 }
