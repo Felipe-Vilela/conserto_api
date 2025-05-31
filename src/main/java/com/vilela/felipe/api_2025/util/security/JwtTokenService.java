a57fc2207268323b62e6a92b2ef755c14bfd7145
Felipe Vilela <felipe.vilela01@outlook.com>
2025-05-31 12:51:02 -0300

    feat: gerenciar tokens

---
+++ src/main/java/com/vilela/felipe/api_2025/util/security/JwtTokenService.java
@@ -0,0 +1,29 @@
+package com.vilela.felipe.api_2025.util.security;
+
+import com.auth0.jwt.JWT;
+import com.auth0.jwt.algorithms.Algorithm;
+import com.auth0.jwt.exceptions.JWTCreationException;
+import com.vilela.felipe.api_2025.usuario.Usuario;
+
+import java.time.Instant;
+import java.time.LocalDateTime;
+import java.time.ZoneOffset;
+
+public class JwtTokenService {
+    public String gerarToken(Usuario usuario) {
+        try {
+            var algoritmo = Algorithm.HMAC256("12345678");
+            return JWT.create()
+                    .withIssuer("DISCIPLINA PW3")
+                    .withSubject(usuario.getLogin())
+                    .withExpiresAt(dataExpiracao())
+                    .sign(algoritmo);
+        } catch (JWTCreationException exception){
+            throw new RuntimeException("Erro ao gerar token jwt", exception);
+        }
+    }
+    private Instant dataExpiracao() {
+        return LocalDateTime.now().plusHours(2).toInstant(ZoneOffset.of("-03:00"));
+    }
+
+}
