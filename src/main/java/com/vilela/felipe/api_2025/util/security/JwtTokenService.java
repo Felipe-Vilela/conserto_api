7b3fc9e8715cd1fa5bb731dedcbb5dd5465588f1
Felipe Vilela <felipe.vilela01@outlook.com>
2025-06-01 10:58:58 -0300

    feat: usuário autenticado

--- src/main/java/com/vilela/felipe/api_2025/util/security/JwtTokenService.java
+++ src/main/java/com/vilela/felipe/api_2025/util/security/JwtTokenService.java
@@ -3,6 +3,7 @@ package com.vilela.felipe.api_2025.util.security;
 import com.auth0.jwt.JWT;
 import com.auth0.jwt.algorithms.Algorithm;
 import com.auth0.jwt.exceptions.JWTCreationException;
+import com.auth0.jwt.exceptions.JWTVerificationException;
 import com.vilela.felipe.api_2025.usuario.Usuario;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
@@ -12,15 +13,13 @@ import java.time.LocalDateTime;
 import java.time.ZoneOffset;
 
 @Service
-public class JwtTokenService {
+public class JwtTokenService<JWTVerifier> {
 
     @Value("${senha.principal.geracao.token}")
     private String secret;
 
     public String gerarToken(Usuario usuario) {
-
         System.out.println(secret);
-
         try {
             var algoritmo = Algorithm.HMAC256(secret);
             return JWT.create()
@@ -32,6 +31,19 @@ public class JwtTokenService {
             throw new RuntimeException("Erro ao gerar token jwt", exception);
         }
     }
+
+    public String getSubject(String tokenJWT) {
+        try {
+            var algoritmo = Algorithm.HMAC256(secret);
+            com.auth0.jwt.JWTVerifier jwtv = JWT.require(algoritmo)
+                    .withIssuer("DISCIPLINA PW3")
+                    .build();
+            return jwtv.verify(tokenJWT).getSubject();
+        } catch (JWTVerificationException exception){
+            throw new RuntimeException("Token JWT inválido ou expirado!");
+        }
+    }
+
     private Instant dataExpiracao() {
         return LocalDateTime.now().plusHours(2).toInstant(ZoneOffset.of("-03:00"));
     }
--- src/main/java/com/vilela/felipe/api_2025/util/security/SecurityConfigurations.java
+++ src/main/java/com/vilela/felipe/api_2025/util/security/SecurityConfigurations.java
@@ -9,15 +9,31 @@ import org.springframework.security.config.annotation.web.configuration.EnableWe
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import static org.springframework.security.web.util.matcher.AntPathRequestMatcher.antMatcher;
+
 @Configuration
 @EnableWebSecurity
 public class SecurityConfigurations {
+
+    private final SecurityFilter securityFilter;
+
+    public SecurityConfigurations(SecurityFilter securityFilter) {
+        this.securityFilter = securityFilter;
+    }
+
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         return http.csrf(csrf -> csrf.disable())
                 .sessionManagement(sm -> sm.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+                .authorizeHttpRequests(req -> {
+                    req.requestMatchers(antMatcher("/login")).permitAll();
+                    req.anyRequest().authenticated();})
+                .addFilterBefore(securityFilter, UsernamePasswordAuthenticationFilter.class)
                 .build();
     }
+
+
 
     @Bean
     public AuthenticationManager authenticationManager(AuthenticationConfiguration configuration)
